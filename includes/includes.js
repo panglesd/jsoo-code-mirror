1a6cb8f6b119988db4dc083438c3ef179881a960
Paul-Elliot <peada@free.fr>
2023-11-25 17:02:07 +0100

    markdown and other things?
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- includes/includes.js
+++ includes/includes.js
@@ -2,6 +2,7 @@ import { EditorView, basicSetup } from "codemirror"
 import { EditorState, ChangeSet } from "@codemirror/state"
 import { hoverTooltip, ViewPlugin } from "@codemirror/view"
 import { receiveUpdates, sendableUpdates, getSyncedVersion, collab } from "@codemirror/collab"
+import { markdown } from "@codemirror/lang-markdown"
 import * as lint from "@codemirror/lint"
 import * as autocomplete from "@codemirror/autocomplete"
 import * as dark from "@codemirror/theme-one-dark"
@@ -14,6 +15,7 @@ joo_global_object.__CM__receiveUpdates = receiveUpdates;
 joo_global_object.__CM__sendableUpdates = sendableUpdates;
 joo_global_object.__CM__getSyncedVersion = getSyncedVersion;
 joo_global_object.__CM__viewPlugin = ViewPlugin;
+joo_global_object.__CM__markdown = markdown;
 joo_global_object.__CM__changeSet = ChangeSet;
 joo_global_object.__CM__state = EditorState;
 joo_global_object.__CM__lint = lint;
